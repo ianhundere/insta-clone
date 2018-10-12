96ba759d9cf5ad5ffbca9d57703dcde70d9aff32
Ian Hundere <ianfundere@gmail.com>
2018-10-12 14:01:05 -0400

    adds images

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-const instaArr = ['https://i.imgur.com/nxUyI.png','imgur.com/QuOfz.png','x.imgur.com/ezN8FGp.jpg','http://x.imgur.com/ONp7H.png',
-'http://x.imgur.com/ptBNQ.png','http://x.imgur.com/5hZHV.png','http://x.imgur.com/usBXZ.png','http://x.imgur.com/oIPtI.png',
-'http://x.imgur.com/Uuya4.png','http://x.imgur.com/L629C.png','http://x.imgur.com/dE8dQ.png'];
+const instaArr = ['images/1.jpeg', 'images/1.jpeg', 'images/1.jpeg','images/1.jpeg',
+    'images/1.jpeg', 'images/1.jpeg', 'images/1.jpeg','images/1.jpeg',
+    'images/1.jpeg', 'images/1.jpeg','images/1.jpeg'];
 
 const outputImg = document.querySelector('[data-img]');
 
